3c681f5edcb7d57fa081f0dd95a74cdf2c140c3a
mcarreon <carryon2095@gmail.com>
2018-07-06 17:22:06 -0700

    added api calls on button list and on submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 var $buttonArea = $('#button-area');
-
 var topicList = [
     "roses",
     "zebra",
@@ -19,10 +18,12 @@ var topicList = [
     "space",
     "fight",
     "cars"
-]
+];
+
+var pastResults = [];
 
 var appCtrl = {
-    fill: function () {
+    fillButtons: function () {
         $buttonArea.empty();
         var buttonPack = $('<div>');
         buttonPack.attr('class', 'button-pack');
@@ -36,24 +37,60 @@ var appCtrl = {
         }
         $buttonArea.append(buttonPack);
     },
+    fillPast: function () {  
+    }
 }
 
 $(document).ready(function () {
-    appCtrl.fill();
+    appCtrl.fillButtons();
+});
+
+$(document).on('click', ".result-buttons", function () {
+    var queryParams = { "api_key": "OYJEVAFS1M734Eb58kq2f8hBFVMJX6Yj" };
+    queryParams.q = $(this).attr('search-data'); 
+
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" + $.param(queryParams);
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+          console.log(response);
+      });
+});
+
+$('#search-button').on('click', function (event) {
+    //doesn't work?!
+    event.preventDefault();
+
+    var searchItem = $('#search-entry').val().trim(); 
+    pastResults.push(searchItem);
+    
+    if (searchItem != '') {
+        var queryParams = { "api_key": "OYJEVAFS1M734Eb58kq2f8hBFVMJX6Yj" };
+        queryParams.q = searchItem; 
+    
+    
+        var queryURL = "https://api.giphy.com/v1/gifs/search?" + $.param(queryParams);
+        console.log(queryURL);
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function (response) {
+              console.log(response);
+          });
+    }
+
+    $('#search-entry').val('');
 });
 
 
 //ajax snippet
-var queryParams = {
-    "api_key": "OYJEVAFS1M734Eb58kq2f8hBFVMJX6Yj"
-}
- 
-var queryURL = "https://api.giphy.com/v1/gifs/search?" + $.param(queryParams);
-console.log(queryURL);
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-      console.log(response);
-  });
+ 
+
+
+
