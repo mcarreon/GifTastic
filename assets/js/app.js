41bc659bbf0a3aa56fbb3e4f6ef00baaf3e326cd
mcarreon <carryon2095@gmail.com>
2018-07-07 19:36:19 -0700

    added favorites functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,10 @@ var topics = [
     "cars"
 ];
 
-var favoritesList = [];
+var favoritesList = JSON.parse(localStorage.getItem('favorites'));
+if (!Array.isArray(favoritesList)) {
+    favoritesList = [];
+}
 
 var pastResults = [];
 
@@ -65,17 +68,20 @@ var appCtrl = {
             var textRating = $('<div>');
             textRating.text(`Rating: ${returnedItems[i].rating}`);
             textRating.attr('class', 'rating card-text');
+            textRating.attr('data', returnedItems[i].rating);
             var textName = $('<div>');
             textName.text(returnedItems[i].title);
             textName.attr('class', 'name card-text');
             var textDate = $('<div>');
             textDate.text(`Date Uploaded: ${returnedItems[i].import_datetime}`);
             textDate.attr('class', 'date card-text');
+            textDate.attr('data', returnedItems[i].import_datetime);
             
             if (returnedItems[i].username != "") {
                 var textUser = $('<div>');
                 textUser.text(`Uploaded By: ${returnedItems[i].username}`);
                 textUser.attr('class', 'username card-text');
+                textUser.attr('data', returnedItems[i].username);
             }
                   
             var img = $('<img>');
@@ -87,12 +93,11 @@ var appCtrl = {
                 'src': returnedItems[i].images.original_still.url
             });
 
-            var fav = $('<button>');
+            var fav = $('<i>');
             fav.attr({
                 'value': `${i}`,
-                'class': 'save-button btn btn-block text-white card-footer'
+                'class': 'save-favorite-button float-right far fa-star'
         });
-            fav.text('Favorite!');
             
             textPack.append(textRating);
             textPack.append(textName);
@@ -100,14 +105,47 @@ var appCtrl = {
             textPack.append(textUser);
 
             imgCard.append(img);
-            imgCard.append(textPack);
             imgCard.append(fav);
+            imgCard.append(textPack);
+            
 
             imgPack.append(imgCard);
         }    
         $resultsArea.append(imgPack);
     },
     fillPast: function () {  
+    },
+    saveFavorite: function (element) {
+        var original = { url: $(element).siblings(".gif-image").attr('data-animate') }; 
+        var original_still = {  url: $(element).siblings(".gif-image").attr('data-still')}
+        
+        var rating = $(element).siblings(".card-body").children('.rating').attr('data');
+        var title = $(element).siblings(".card-body").children('.name').text();
+        var import_datetime = $(element).siblings(".card-body").children('.date').attr('data');
+        var username = $(element).siblings(".card-body").children('.username').attr('data');
+    
+        
+    
+        console.log(rating);
+        console.log(title);
+        console.log(import_datetime);
+        console.log(username);
+        console.log(original);
+    
+        var favorite = {
+            rating: rating,
+            title: title,
+            import_datetime: import_datetime,
+            username: username,
+            images: {
+                original: original,
+                original_still: original_still
+            }
+        }
+    
+        console.log(favorite);
+
+        return favorite;
     },
     buildUrl: function (search) {
         var queryParams = { 
@@ -174,12 +212,20 @@ $('#search-button').on('click', function (event) {
 });
 
 $(document).on('click', '.favorite-buttons', function () {
+    appCtrl.clearResults();
+    appCtrl.fillResults(favoritesList);
+    $('.save-favorite-button').remove();
+});
 
-})
+$(document).on('click', '.save-favorite-button', function () {
+    $(this).css({color: '#343a40'});
+    favoritesList.push(appCtrl.saveFavorite(this));
+    localStorage.clear();
+    localStorage.setItem('favorites', JSON.stringify(favoritesList));
+    console.log(favoritesList);
 
-$(document).on('click', '.save-button', function () {
     
-})
+});
 
 $(document).on("click", ".gif-image", function() {
     
@@ -192,6 +238,11 @@ $(document).on("click", ".gif-image", function() {
       $(this).attr("src", $(this).attr("data-still"));
       $(this).attr("data-state", "still");
     }
+});
+
+$('.clear-fav').on('click', function () {
+    localStorage.clear();
+    favoritesList = [];
 });
 
 
